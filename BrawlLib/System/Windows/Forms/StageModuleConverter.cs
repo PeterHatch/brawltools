0d561f5dc629966c6e88e4fd4a7af8a0fa577cdb
libertyernie <maybeway36@gmail.com>
2014-06-27 22:07:13 -0500

    Try to find all module ID offsets programatically (attempt to future-proof the stage module converter)

--- BrawlLib/System/Windows/Forms/StageModuleConverter.cs
+++ BrawlLib/System/Windows/Forms/StageModuleConverter.cs
@@ -7,93 +7,100 @@ using System.Collections.ObjectModel;
 
 namespace System.Windows.Forms
 {
-    //Class coded by libertyernie.
     public class StageModuleConverter : Form
     {
         public const string FILTER = "Module files (*.rel)|*.rel";
 
         #region Definition of "Stage" inner class
-        public class Stage
-        {
-            private byte id;
-            private string name;
-            private string filename;
+        public class Stage {
+            /// <summary>
+            /// The stage ID, as used by the module files and the Custom SSS code.
+            /// </summary>
+            public byte ID { get; private set; }
+            /// <summary>
+            /// The stage name (e.g. "Flat Zone 2").
+            /// </summary>
+            public string Name { get; private set; }
+            /// <summary>
+            /// The .rel filename (e.g. "st_gw.rel").
+            /// </summary>
+            public string RelName { get; private set; }
+            /// <summary>
+            /// The name of the .pac file, minus "STG" and anything    
+            /// after an underscore (e.g. "gw" or "emblem").
+            /// </summary>
+            public string PacBasename { get; private set; }
 
-            public byte ID { get { return id; } }
-            public string Name { get { return name; } }
-            public string Filename { get { return filename; } }
+            public bool ContainsPac(string filename) {
+                int i = filename.IndexOfAny(new char[] { '.', '_' });
+                if (filename.Length < 3 || i < 0) return false;
 
-            public Stage(byte id, string name, string filename)
-            {
-                this.id = id;
-                this.name = name;
-                this.filename = filename;
+                string input_basename = filename.Substring(3, i - 3);
+                return String.Equals(input_basename.ToLower(), PacBasename.ToLower(), StringComparison.InvariantCultureIgnoreCase);
             }
 
-            public override string ToString() { return name; }
+            public Stage(byte id, string name, string relname, string pac_basename) {
+                this.ID = id;
+                this.Name = name;
+                this.RelName = relname;
+                this.PacBasename = pac_basename;
+            }
+
+            public override string ToString() { return Name; }
         }
         #endregion
 
-		private static Stage[] stageList = new Stage[] {
-			new Stage(0, "STGCUSTOM##.pac", "st_custom##.rel"),
-			new Stage(1, "Battlefield", "st_battle.rel"),
-			new Stage(2, "Final Destination", "st_final.rel"),
-			new Stage(3, "Delfino Plaza", "st_dolpic.rel"),
-			new Stage(4, "Luigi's Mansion", "st_mansion.rel"),
-			new Stage(5, "Mushroomy Kingdom", "st_mariopast.rel"),
-			new Stage(6, "Mario Circuit", "st_kart.rel"),
-			new Stage(7, "75 m", "st_donkey.rel"),
-			new Stage(8, "Rumble Falls", "st_jungle.rel"),
-			new Stage(9, "Pirate Ship", "st_pirates.rel"),
-			new Stage(11, "Norfair", "st_norfair.rel"),
-			new Stage(12, "Frigate Orpheon", "st_orpheon.rel"),
-			new Stage(13, "Yoshi's Island (Brawl)", "st_crayon.rel"),
-			new Stage(14, "Halberd", "st_halberd.rel"),
-			new Stage(19, "Lylat Cruise", "st_starfox.rel"),
-			new Stage(20, "Pokemon Stadium 2", "st_stadium.rel"),
-			new Stage(21, "Spear Pillar", "st_tengan.rel"),
-			new Stage(22, "Port Town Aero Dive", "st_fzero.rel"),
-			new Stage(23, "Summit", "st_ice.rel"),
-			new Stage(24, "Flat Zone 2", "st_gw.rel"),
-			new Stage(25, "Castle Siege", "st_emblem.rel"),
-			new Stage(28, "WarioWare Inc.", "st_madein.rel"),
-			new Stage(29, "Distant Planet", "st_earth.rel"),
-			new Stage(30, "Skyworld", "st_palutena.rel"),
-			new Stage(31, "Mario Bros.", "st_famicom.rel"),
-			new Stage(32, "New Pork City", "st_newpork.rel"),
-			new Stage(33, "Smashville", "st_village.rel"),
-			new Stage(34, "Shadow Moses Island", "st_metalgear.rel"),
-			new Stage(35, "Green Hill Zone", "st_greenhill.rel"),
-			new Stage(36, "PictoChat", "st_pictchat.rel"),
-			new Stage(37, "Hanenbow", "st_plankton.rel"),
-			new Stage(38, "ConfigTest", "st_config.rel"),
-			new Stage(41, "Temple", "st_dxshrine.rel"),
-			new Stage(42, "Yoshi's Island (Melee)", "st_dxyorster.rel"),
-			new Stage(43, "Jungle Japes", "st_dxgarden.rel"),
-			new Stage(44, "Onett", "st_dxonett.rel"),
-			new Stage(45, "Green Greens", "st_dxgreens.rel"),
-			new Stage(46, "Pokemon Stadium", "st_dxpstadium.rel"),
-			new Stage(47, "Rainbow Cruise", "st_dxrcruise.rel"),
-			new Stage(48, "Corneria", "st_dxcorneria.rel"),
-			new Stage(49, "Big Blue", "st_dxbigblue.rel"),
-			new Stage(50, "Brinstar", "st_dxzebes.rel"),
-			new Stage(51, "Bridge of Eldin", "st_oldin.rel"),
-			new Stage(52, "Homerun", "st_homerun.rel"),
-			new Stage(53, "Edit", "st_stageedit.rel"),
-			new Stage(54, "Heal", "st_heal.rel"),
-			new Stage(55, "Online Training", "st_otrain.rel"),
-			new Stage(56, "TargetBreak", "st_tbreak.rel"),
-		};
-		private static int[] indicesToIgnore = {
-			2959, // st_croll (PAL)
-			431, // st_onett, st_metalgear
-			387, // st_dxyorster
-			2519, // st_croll (NTSC)
-			419, // st_donkey
-			423, // st_halberd, st_jungle, st_mansion
-			};
-		public static ReadOnlyCollection<Stage> StageList { get { return Array.AsReadOnly(stageList); } }
-		public static ReadOnlyCollection<int> IndicesToIgnore { get { return Array.AsReadOnly(indicesToIgnore); } }
+        public readonly static Stage[] Stages = new Stage[] {
+            //        ID    Display Name              .rel filename        Name without STG
+            new Stage(0x00, "STGCUSTOM##.pac",        "st_custom##.rel",   "custom"),
+            new Stage(0x01, "Battlefield",            "st_battle.rel",     "battlefield"),
+            new Stage(0x02, "Final Destination",      "st_final.rel",      "final"),
+            new Stage(0x03, "Delfino Plaza",          "st_dolpic.rel",     "dolpic"),
+            new Stage(0x04, "Luigi's Mansion",        "st_mansion.rel",    "mansion"),
+            new Stage(0x05, "Mushroomy Kingdom",      "st_mariopast.rel",  "mariopast"),
+            new Stage(0x06, "Mario Circuit",          "st_kart.rel",       "kart"),
+            new Stage(0x07, "75 m",                   "st_donkey.rel",     "donkey"),
+            new Stage(0x08, "Rumble Falls",           "st_jungle.rel",     "jungle"),
+            new Stage(0x09, "Pirate Ship",            "st_pirates.rel",    "pirates"),
+            new Stage(0x0B, "Norfair",                "st_norfair.rel",    "norfair"),
+            new Stage(0x0C, "Frigate Orpheon",        "st_orpheon.rel",    "orpheon"),
+            new Stage(0x0D, "Yoshi's Island (Brawl)", "st_crayon.rel",     "crayon"),
+            new Stage(0x0E, "Halberd",                "st_halberd.rel",    "halberd"),
+            new Stage(0x13, "Lylat Cruise",           "st_starfox.rel",    "starfox"),
+            new Stage(0x14, "Pokemon Stadium 2",      "st_stadium.rel",    "stadium"),
+            new Stage(0x15, "Spear Pillar",           "st_tengan.rel",     "tengan"),
+            new Stage(0x16, "Port Town Aero Dive",    "st_fzero.rel",      "fzero"),
+            new Stage(0x17, "Summit",                 "st_ice.rel",        "ice"),
+            new Stage(0x18, "Flat Zone 2",            "st_gw.rel",         "gw"),
+            new Stage(0x19, "Castle Siege",           "st_emblem.rel",     "emblem"),
+            new Stage(0x1C, "WarioWare Inc.",         "st_madein.rel",     "madein"),
+            new Stage(0x1D, "Distant Planet",         "st_earth.rel",      "earth"),
+            new Stage(0x1E, "Skyworld",               "st_palutena.rel",   "palutena"),
+            new Stage(0x1F, "Mario Bros.",            "st_famicom.rel",    "famicom"),
+            new Stage(0x20, "New Pork City",          "st_newpork.rel",    "newpork"),
+            new Stage(0x21, "Smashville",             "st_village.rel",    "village"),
+            new Stage(0x22, "Shadow Moses Island",    "st_metalgear.rel",  "metalgear"),
+            new Stage(0x23, "Green Hill Zone",        "st_greenhill.rel",  "greenhill"),
+            new Stage(0x24, "PictoChat",              "st_pictchat.rel",   "pictchat"),
+            new Stage(0x25, "Hanenbow",               "st_plankton.rel",   "plankton"),
+            new Stage(0x26, "ConfigTest",             "st_config.rel",     "configtest"),
+            new Stage(0x29, "Temple",                 "st_dxshrine.rel",   "dxshrine"),
+            new Stage(0x2A, "Yoshi's Island (Melee)", "st_dxyorster.rel",  "dxyorster"),
+            new Stage(0x2B, "Jungle Japes",           "st_dxgarden.rel",   "dxgarden"),
+            new Stage(0x2C, "Onett",                  "st_dxonett.rel",    "dxonett"),
+            new Stage(0x2D, "Green Greens",           "st_dxgreens.rel",   "dxgreens"),
+            new Stage(0x2E, "Pokemon Stadium",        "st_dxpstadium.rel", "dxpstadium"),
+            new Stage(0x2F, "Rainbow Cruise",         "st_dxrcruise.rel",  "dxrcruise"),
+            new Stage(0x30, "Corneria",               "st_dxcorneria.rel", "dxcorneria"),
+            new Stage(0x31, "Big Blue",               "st_dxbigblue.rel",  "dxbigblue"),
+            new Stage(0x32, "Brinstar",               "st_dxzebes.rel",    "dxzebes"),
+            new Stage(0x33, "Bridge of Eldin",        "st_oldin.rel",      "oldin"),
+            new Stage(0x34, "Homerun",                "st_homerun.rel",    "homerun"),
+            new Stage(0x35, "Edit",                   "st_stageedit.rel",  "edit"),
+            new Stage(0x36, "Heal",                   "st_heal.rel",       "heal"),
+            new Stage(0x37, "Online Training",        "st_otrain.rel",     "onlinetraining"),
+            new Stage(0x38, "TargetBreak",            "st_tbreak.rel",     "targetlv"),
+        };
 
         #region Designer
 
@@ -119,11 +126,11 @@ namespace System.Windows.Forms
         private Label lblSizeDesc;
         private Label lblNameValue;
         private Button btnBrowse;
-		private Label lblIDValue;
-		private Label lblIDDesc;
-		private Label label3;
-		private ComboBox itemSelection;
-		private Label lblItemDesc;
+        private Label lblIDValue;
+        private Label lblIDDesc;
+        private Label label3;
+        private ComboBox itemSelection;
+        private Label lblItemDesc;
 
         private void InitializeComponent()
         {
@@ -535,7 +542,7 @@ namespace System.Windows.Forms
         {
             InitializeComponent();
 
-            stageSelection.Items.AddRange(stageList);
+            stageSelection.Items.AddRange(Stages);
             stageSelection.SelectedIndex = 0;
 
             #region Tooltips
@@ -623,13 +630,13 @@ namespace System.Windows.Forms
             byte currentID = findCurrentID();
             lblIDValue.Text = "0x" + Convert.ToString(currentID, 16);
 
-			foreach (Stage s in stageList) {
-				if (s.ID == currentID) {
-					lblCurrentStage.Text = s.Name;
-					stageSelection.SelectedItem = s;
-					break;
-				}
-			}
+            foreach (Stage s in Stages) {
+                if (s.ID == currentID) {
+                    lblCurrentStage.Text = s.Name;
+                    stageSelection.SelectedItem = s;
+                    break;
+                }
+            }
 
             if (lblNameValue.Text.StartsWith("stOnline"))
             {
@@ -692,7 +699,7 @@ namespace System.Windows.Forms
                     _data[offsets[i]] = b;
                 }
             }
-            _outputName = s.Filename;
+            _outputName = s.RelName;
 
             DialogResult = DialogResult.OK;
             Close();
@@ -702,33 +709,21 @@ namespace System.Windows.Forms
         {
             // search through pointer
             int length = _data.Length;
-            byte[] searchFor = { 0x38, 0xa5, 0x00, 0x00, 0x38, 0x80, 0x00 };
-            int indexToCheck = 0;
-            bool found = false;
+            byte?[] searchFor = { 0x38, null, 0x00, null,
+                                  0x38, 0xA5, 0x00, 0x00,
+                                  0x38, 0x80, 0x00 };
 
-            int i = 0;
-            while (!found && i < length)
-            {
-                if (_data[i] == searchFor[indexToCheck])
-                {
+            int indexToCheck = 0;
+            for (int i = 0; i < length; i++) {
+                byte? b = searchFor[indexToCheck];
+                if ((b ?? _data[i]) == _data[i]) {
                     indexToCheck++;
-                    if (indexToCheck == searchFor.Length)
-                        if (IndicesToIgnore.Contains(i + 1))
-                        {
-                            //MessageBox.Show("ignored " + (i + 1));
-                            indexToCheck = 0;
-                        }
-                        else
-                            found = true;
-                }
-                else
+                    if (indexToCheck == searchFor.Length) return i + 1;
+                } else {
                     indexToCheck = 0;
-                i++;
+                }
             }
-            if (found)
-                return i;
-            else
-                return -1;
+            return -1;
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
